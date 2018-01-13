fd503fcbde99f0139e45792e01b0affbbb675b93
thuanhdo <trahaanh2411@gmail.com>
2018-01-13 11:19:45 +0700

    Labwork 4

---
+++ 04.practical.work.fork.exec.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<unistd.h>
+int main(){
+printf("Main before fork()\n");
+int pid0 = fork();
+
+if(pid0==0){
+int pid = fork();
+
+if(pid!=0){
+printf("1st child, launching ps -ef\n");
+char *args[] = {"/bin/ps", "-ef", NULL};
+execvp("/bin/ps", args);
+}
+
+else{
+printf("2nd child, launching free -h\n");
+char *args[] = {"/bin/free","-h", NULL};
+execvp("/bin/free", args);
+}
+}
+}
+
