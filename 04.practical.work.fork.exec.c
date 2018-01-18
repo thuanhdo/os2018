1b6dde45eaeacfb9e83675f7ea0a04ab02eddd25
thuanhdo <trahaanh2411@gmail.com>
2018-01-18 15:45:58 +0700

    Update lining

--- 04.practical.work.fork.exec.c
+++ 04.practical.work.fork.exec.c
@@ -11,12 +11,14 @@ if(pid0==0){
 		printf("1st child, launching ps -ef\n");
 		char *args[] = {"/bin/ps", "-ef", NULL};
 		execvp("/bin/ps", args);
+		printf("check1");
 	}
 
 	else{
-	printf("2nd child, launching free -h\n");
-	char *args[] = {"/bin/free","-h", NULL};
-	execvp("/bin/free", args);
+		printf("2nd child, launching free -h\n");
+		char *args[] = {"/bin/free","-h", NULL};
+		execvp("/bin/free", args);
+		printf("check2");
 	}
 }
 }
